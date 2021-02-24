b9a743385c741e9beb2d57a6fe23a547677ccf05
Hernany Dias <contatohernany@gmail.com>
2021-02-23 21:06:18 -0300

    Criando e estilizando a Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/images/github-logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashborad</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28400751?s=400&u=2ecb3177dc614808114acb163ff8d79768332369&v=4"
+            alt="Hernany Dias"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28400751?s=400&u=2ecb3177dc614808114acb163ff8d79768332369&v=4"
+            alt="Hernany Dias"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28400751?s=400&u=2ecb3177dc614808114acb163ff8d79768332369&v=4"
+            alt="Hernany Dias"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/28400751?s=400&u=2ecb3177dc614808114acb163ff8d79768332369&v=4"
+            alt="Hernany Dias"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
